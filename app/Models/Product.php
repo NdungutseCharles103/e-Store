e5fd6d349325c21d137e3ce02fa05da67b219154
Ndungutse Charles <ndungutsecharles103@gmail.com>
2022-12-17 17:12:51 +0200

    made core start changes

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+    
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'p_name',
+        'p_qoh',
+        'price',
+        'p_type',
+        'u_id',
+    ];
+    
+}
+?>
